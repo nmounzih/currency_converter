a0c10b5d2139a8abf230905eb902bb49085ebb57
Nadia Mounzih <nmounzih@yahoo.com>
2017-02-28 08:57:57 -0500

    Fixed formula for converter equation

--- currency_converter.py
+++ currency_converter.py
@@ -1,4 +1,4 @@
-import Currency
+from currency import Currency
 
 dict_rates = {'USD': 1.0, 'EUR': 0.94, 'GBP': 0.80}
 
@@ -8,20 +8,15 @@ class UnknownCurrencyCodeError(ValueError):
 
 
 class CurrencyConverter:
-    def __init__(self, dict_rates, code):
+    def __init__(self, dict_rates, code, amount):
+        self.dict_rates = dict_rates
         self.code = code
         self.amount = amount
-        self.dict_rates = dict_rates
 
-    def convert(self, currency_obj, currency_to_convert):
-        if currency_to_convert not in self.dict_rates:
+    def convert(self, currency_obj, code_to_convert):
+        if code_to_convert not in self.dict_rates:
             raise UnknownCurrencyCodeError
-        elif currency_obj.code == currency_to_convert:
+        elif currency_obj.code == code_to_convert:
             return currency_obj
         else:
-                if currency_to_convert == 'GBP':
-                    return (0.8 * currency_obj.amount)
-                elif currency_to_convert == 'EUR':
-                    return (0.94 * currency_obj.amount)
-                elif currency_to_convert == 'USD':
-                    return(1.0 * currency_obj.amount)
+            return (currency_obj.amount * self.dict_rates[currency_obj.code]/self.code)
