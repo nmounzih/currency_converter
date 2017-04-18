4e3f1bd5fe85451b5b89b854d6c1fa822e942ed6
Nadia Mounzih <nmounzih@yahoo.com>
2017-04-18 15:33:41 -0400

    Fixed init, fixed re.sub, fixed __sub__

--- currency.py
+++ currency.py
@@ -7,34 +7,37 @@ class DifferentCurrencyCodeError(ValueError):
 
 class Currency:
 
-    known_codes = {"USD": "$", "GBP": "£", "JPY": "¥"}
     known_symbols = {"$": "USD", "£": "GBP", "¥": "JPY"}
 
     def __init__(self, amount, code=""):
-        self.code = code
-        self.amount = re.sub(r'[^$£€B]', "", float(amount))
+        if code == '':
+            self.code = self.known_symbols[amount[0]]
+            amount = re.sub(r'[$£€]', "", amount)
+        else:
+            self.code = code
+        self.amount = float(amount)
 
     def __repr__(self):
-        return "{}{}".format(self.amount, self.code)
+        return "{}, {}".format(self.amount, self.code)
 
     def __eq__(self, other):
         return self.code == other.code and self.amount == other.amount
 
     def __add__(self, other):
         if self.code == other.code:
-            return self.amount + other.amount
+            return Currency(self.amount + other.amount, self.code)
         else:
             raise DifferentCurrencyCodeError("different currencies")
 
     def __sub__(self, other):
         if self.code == other.code:
-            return (self.amount - other.amount) or (other.amount - self.amount)
+            return Currency(self.amount - other.amount, self.code)
         else:
             raise DifferentCurrencyCodeError("different currencies")
 
     def __mul__(self, other):
         if type(other) == float or type(other) == int:
-            return self.amount * other, self.code
+            return Currency(self.amount * other, self.code)
         else:
             raise TypeError
 
