f5a5808ecf3b994ba86f3552a43e7e8f0bd76e72
Nadia Mounzih <nmounzih@yahoo.com>
2017-04-18 15:31:19 -0400

    Rewrote tests to test more cases and apply to fixed converter

--- currency_tests.py
+++ currency_tests.py
@@ -1,16 +1,18 @@
 from currency import Currency
-from currency import Currency, DifferentCurrencyCodeError
+from currency import DifferentCurrencyCodeError
+from currency_converter import CurrencyConverter
 from nose.tools import raises
 
-a = Currency("USD", "5.00")
-b = Currency("USD", "4.50")
+a = Currency("5.00", "USD")
+b = Currency("4.50", "USD")
 f = a
-c = Currency("GBP", "7")
-d = Currency("USD", "1")
-e = Currency("USD", "6.00")
+c = Currency("7", "GBP")
+d = Currency("1", "USD")
+e = Currency("6.00", "USD")
 g = 2
 h = 5.0
-
+i = Currency("0.94", "EUR")
+j = Currency("1", "EUR")
 
 def test_equal():
     assert (a == f) == True
@@ -18,17 +20,19 @@ def test_equal():
 
 
 def test_addition():
-    assert a + b == 9.5
-    assert a + d == 6
+    assert a + b == Currency(9.50, "USD")
+    assert a + d == Currency(6, "USD")
 
 
 def test_subtraction():
-    assert a.sub(b) == 0.50
-    assert e.sub(a) == 1
+    assert a - b == Currency(0.50, "USD")
+    assert e - a == Currency(1, "USD")
+
 
 def test_multiply():
-    assert a * g == 10.00
-    assert a * h == 25
+    assert a * g == Currency(10.00, "USD")
+    assert a * h == Currency(25, "USD")
+
 
 @raises(DifferentCurrencyCodeError)
 def test_different_currency_code_error():
@@ -36,7 +40,17 @@ def test_different_currency_code_error():
     a - c
 
 
+def test_conversion():
+    dict_rates = {'USD': 1.0, 'EUR': 0.94, 'GBP': 0.80}
+    c = CurrencyConverter(dict_rates)
+    assert c.convert(a, "USD") == Currency("5.00", "USD")
+    assert c.convert(d, "EUR") == Currency(0.94, "EUR")
+    assert c.convert(i, "USD") == Currency(1, "USD")
+    assert c.convert(j, "USD") != Currency(1, "USD")
+
+
 
 test_equal()
 test_addition()
 test_subtraction()
+test_multiply()
